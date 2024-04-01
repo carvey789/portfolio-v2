669f829b5f73a897285743a91b26c22298fae725
carvey789 <carvey789@gmail.com>
2024-04-01 21:34:55 +0700

    style: add blob

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "white-main": "#f8f7f4",
+        "black-main": "#1c1c1c",
+        "black-nav": "#0f0f11",
+      },
+    },
+  },
+  plugins: [],
+};
