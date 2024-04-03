d893e1956656eadc0b59736a9d155c8c8d444316
carvey789 <carvey789@gmail.com>
2024-04-03 07:37:37 +0700

    chore: adjust layout and nav width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { useMediaQuery } from "../hooks/useMediaQuery";
-
-interface NavbarProps {
-  activePath: string;
-}
 
 interface HamburgerProps {
   toggled: boolean;
@@ -13,7 +9,6 @@ interface HamburgerProps {
 
 interface MobileNavProps {
   toggled: boolean;
-  activePath: string;
   navlist: {
     title: string;
     href: string;
@@ -53,7 +48,7 @@ function Hamburger({ toggled, onClick }: HamburgerProps) {
   );
 }
 
-function MobileNav({ navlist, toggled, activePath }: MobileNavProps) {
+function MobileNav({ navlist, toggled }: MobileNavProps) {
   return (
     <nav
       className={`absolute top-0 left-0 flex items-center justify-center w-1/3 h-screen px-6 bg-black-nav backdrop-blur-lg transition-transform ${
@@ -63,10 +58,7 @@ function MobileNav({ navlist, toggled, activePath }: MobileNavProps) {
       <ul className="flex flex-col gap-8">
         {navlist.map((nav) => (
           <li key={nav.href} className="group text-xs">
-            <a
-              className={activePath !== nav.href ? "text-white-main/40" : ""}
-              href={nav.href}
-            >
+            <a href={nav.href}>
               {nav.title}
               <span className="text-lg pl-1 transition-opacity opacity-0 group-hover:opacity-100">
                 •
@@ -79,18 +71,13 @@ function MobileNav({ navlist, toggled, activePath }: MobileNavProps) {
   );
 }
 
-export default function Navbar({ activePath }: NavbarProps) {
-  const [currPath, setCurrPath] = useState(activePath);
+export default function Navbar() {
   const [toggled, setToggled] = useState(false);
   const matches = useMediaQuery("(min-width: 1024px)");
 
-  useEffect(() => {
-    if (currPath !== activePath) setCurrPath(activePath);
-  }, [activePath]);
-
   return (
-    <div className="fixed w-screen z-50 max-w-[1440px]">
-      <header className="relative flex items-center justify-between py-4 px-6 bg-black-nav/80 lg:px-[120px] backdrop-blur-lg">
+    <div className="fixed w-screen bg-black-nav/80 z-50 py-4 px-6 lg:px-[120px]">
+      <header className="relative flex items-center justify-between backdrop-blur-lg max-w-[1440px] mx-auto">
         <div className="flex items-center gap-4">
           {!matches && (
             <Hamburger
@@ -104,24 +91,13 @@ export default function Navbar({ activePath }: NavbarProps) {
             </span>
           </a>
         </div>
-        {!matches && (
-          <MobileNav
-            toggled={toggled}
-            navlist={navitems}
-            activePath={currPath}
-          />
-        )}
+        {!matches && <MobileNav toggled={toggled} navlist={navitems} />}
         {matches && (
           <nav>
             <ul className="flex gap-20">
               {navitems.map((nav) => (
                 <li key={nav.href} className="group text-sm lg:text-base">
-                  <a
-                    className={
-                      currPath !== nav.href ? "text-white-main/40" : ""
-                    }
-                    href={nav.href}
-                  >
+                  <a href={nav.href}>
                     {nav.title}
                     <span className="text-lg pl-1 transition-opacity opacity-0 group-hover:opacity-100">
                       •
